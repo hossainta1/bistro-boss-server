cfde01cb0c516c78fbb4ce42dfc7d1be9d88b8f0
hossainta1 <tanvir.hossain303030@gmail.com>
2023-12-09 21:12:53 +0600

    Create admin api

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
-        })
+        });
 
 
         app.post('/users', async (req, res) => {
@@ -52,6 +52,22 @@ async function run() {
                 return res.send({ message: "User already exists" })
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await usersCollection.updateOne(filter, updateDoc);
+
             res.send(result);
         })
 
