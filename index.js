66b2c2947f53cda7c03c57ffa3454b260bf15773
hossainta1 <tanvir.hossain303030@gmail.com>
2023-12-09 18:33:45 +0600

    create user Api for sotre user info in database

--- index.js
+++ index.js
@@ -30,9 +30,18 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const usersCollection = client.db("bistroDb").collection("users");
         const menuCollection = client.db("bistroDb").collection("menu");
         const reviewCollection = client.db("bistroDb").collection("reviews");
         const cartCollection = client.db("bistroDb").collection("carts");
+
+
+        // users related apis
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
         //menu data collection 
         app.get('/menu', async (req, res) => {
